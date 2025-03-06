6a7b9b905b6e825df12018d6b2407a2583f98ebd
Nishant Bhagat <nishantbhagat57@gmail.com>
2025-03-06 20:58:58 +0530

    Update dnstake.go

--- pkg/dnstake/dnstake.go
+++ pkg/dnstake/dnstake.go
@@ -1,6 +1,9 @@
 package dnstake
 
-import "github.com/projectdiscovery/retryabledns"
+import (
+	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/retryabledns"
+)
 
 // Verify if the response is not NOERROR
 func Verify(IPs []string, hostname string) (bool, error) {
@@ -38,3 +41,39 @@ func Verify(IPs []string, hostname string) (bool, error) {
 
 	return false, nil
 }
+
+// Directly check if a domain is vulnerable via CNAME
+func VerifyCNAME(client *retryabledns.Client, hostname string) (bool, string, error) {
+	// Get CNAME record
+	res, err := Resolve(client, hostname, 5) // Type 5 is CNAME
+	if err != nil {
+		gologger.Debug().Msgf("Error getting CNAME for %s: %v", hostname, err)
+		return false, "", err
+	}
+
+	// If there's a CNAME record
+	if len(res.CNAME) > 0 {
+		target := res.CNAME[0]
+		gologger.Debug().Msgf("Found CNAME %s -> %s", hostname, target)
+
+		// Check if target is resolvable
+		targetRes, err := Resolve(client, target, 1) // Type 1 is A record
+		if err != nil {
+			gologger.Debug().Msgf("Error resolving CNAME target %s: %v", target, err)
+			// If we can't resolve, it might be vulnerable
+			return true, target, nil
+		}
+
+		// If target returns SERVFAIL or REFUSED, it's vulnerable
+		if targetRes.StatusCode == "SERVFAIL" || targetRes.StatusCode == "REFUSED" {
+			gologger.Debug().Msgf("CNAME target %s returned %s - VULNERABLE!", target, targetRes.StatusCode)
+			return true, target, nil
+		}
+
+		gologger.Debug().Msgf("CNAME target %s returned %s - not vulnerable", target, targetRes.StatusCode)
+		return false, target, nil
+	}
+
+	return false, "", nil
+}
+
